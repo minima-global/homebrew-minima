1445223787ce1af52b91c95e9eccd808cdfd3462
mcksn <mackson.ogu@gmail.com>
2021-11-18 19:51:59 +0000

    allowing for log pipping when calling minima directly

--- minima.rb
+++ minima.rb
@@ -10,11 +10,11 @@ class Minima < Formula
       	s.gsub! "##PREFIX##", "#{prefix}"
       	s.gsub! "##HOMEBREW_PREFIX##", "#{HOMEBREW_PREFIX}"
       	s.gsub! "##HOMEBREW_PREFIX_ETC##", "#{etc}"
-      	s.gsub! "##HOMEBREW_PREFIX_LOG##", "#{var}/log"
       end      
       inreplace "brew/minima.plist" do |s|
       	s.gsub! "##BIN##", "#{bin}"
       	s.gsub! "##PLIST_NAME##", "#{plist_name}"
+        s.gsub! "##HOMEBREW_PREFIX_LOG##", "#{var}/log"
       end      
       prefix.install "brew/minima.plist", "jar/minima.jar" 
       prefix.install_symlink "minima.plist" => "#{plist_name}.plist"
